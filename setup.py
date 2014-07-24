2c0ae282ed3f8df0caace5b893886bd3fdeb463e
shaief <shaief@gmail.com>
2014-07-24 15:34:37 +0300

    a setup.py was created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='beaverAutoCAD',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/shaief/beaverAutoCAD',
+    license='GPL',
+    author='shaief',
+    author_email='shaief@gmail.com',
+    description='  A small app that collects data from an AutoCAD DWG and exports it to a MS-Excel table'
+)
